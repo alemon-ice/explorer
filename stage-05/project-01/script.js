fa801729a7e0c1312847c13768c6112073c21390
Rafael Dias <rafaeldiasdesouza.rd@gmail.com>
2024-05-01 15:47:53 -0300

    stage-05 - proj-01: finish project

--- stage-05/project-01/script.js
+++ stage-05/project-01/script.js
@@ -1,16 +1,55 @@
-const randomNumber = Math.round(Math.random() * 10)
+let randomNumber = generateRandomNumber()
 let totalAttempts = 0
+const screen1 = document.querySelector('.screen1')
+const screen2 = document.querySelector('.screen2')
+const btnTry = document.querySelector('#btnTry')
+const btnReset = document.querySelector('#btnReset')
 
-function handleClick(event) {
+btnTry.addEventListener('click', handleTryClick)
+btnReset.addEventListener('click', handleResetClick)
+document.addEventListener('keydown', handleKeyDownPress)
+
+function generateRandomNumber() {
+    return Math.round(Math.random() * 10)
+}
+
+function handleTryClick(event) {
     event.preventDefault()
     
     totalAttempts++
     
-    const inputNumber = document.querySelector('#inputNumber').value
+    const input = document.querySelector('#inputNumber')
 
-    if (Number(inputNumber) === randomNumber) {
-        document.querySelector('.screen1').classList.add('hide')
+    const inputNumberValue = Number(input.value)
+
+    if (!inputNumberValue || inputNumberValue < 0 || inputNumberValue > 10) {
+        alert('Número inválido, escolha entre 0 e 10')
+    }
+    else if (Number(inputNumberValue) === randomNumber) {
         document.querySelector('.screen2 h2').innerText = `Acertou em ${totalAttempts} tentativa(s)`
-        document.querySelector('.screen2').classList.remove('hide')
+        toggleScreen()
+    } else {
+        alert('Não foi dessa vez, tente novamente')
+    }
+    input.value = '0'
+}
+
+function handleResetClick(event) {
+    event?.preventDefault()
+
+    randomNumber = generateRandomNumber()
+    totalAttempts = 0
+
+    toggleScreen()
+}
+
+function toggleScreen() {
+    screen1.classList.toggle('hide')
+    screen2.classList.toggle('hide')
+}
+
+function handleKeyDownPress (event) {
+    if (event.key == 'Enter' && screen1.classList.contains('hide')) {
+        handleResetClick()
     }
 }
