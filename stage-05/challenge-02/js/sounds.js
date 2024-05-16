f717c07facca3451bd94b4d4e804807d2c3baba2
Rafael Dias <rafaeldiasdesouza.rd@gmail.com>
2024-05-16 20:58:43 -0300

    stage-05 - challenge-02

---
+++ stage-05/challenge-02/js/sounds.js
@@ -0,0 +1,61 @@
+import {
+    buttonSoundForest,
+    buttonSoundRain,
+    buttonSoundCoffeeShop,
+    buttonSoundFireplace
+} from "./elements.js"
+
+export default function() {
+    const buttonPressAudio = new Audio("./assets/audios/button-press.wav")
+    const kitchenTimer = new Audio("./assets/audios/kichen-timer.mp3")
+    const bgAudioForest = new Audio("./assets/audios/forest.wav")
+    const bgAudioRain = new Audio("./assets/audios/rain.wav")
+    const bgAudioCoffeeShop = new Audio("./assets/audios/coffee-shop.wav")
+    const bgAudioFireplace = new Audio("./assets/audios/fireplace.wav")
+
+    bgAudioForest.loop = true
+    bgAudioRain.loop = true
+    bgAudioCoffeeShop.loop = true
+    bgAudioFireplace.loop = true
+    
+    function pressButton() {
+        buttonPressAudio.play()
+    }
+
+    function timeEnd() {
+        kitchenTimer.play()
+    }
+
+    function audioForest() {
+        buttonSoundForest.classList.contains('active')
+        ? bgAudioForest.play()
+        : bgAudioForest.pause()
+    }
+
+    function audioRain() {
+        buttonSoundRain.classList.contains('active')
+        ? bgAudioRain.play()
+        : bgAudioRain.pause()
+    }
+
+    function audioCoffeeShop() {
+        buttonSoundCoffeeShop.classList.contains('active')
+        ? bgAudioCoffeeShop.play()
+        : bgAudioCoffeeShop.pause()
+    }
+
+    function audioFireplace() {
+        buttonSoundFireplace.classList.contains('active')
+        ? bgAudioFireplace.play()
+        : bgAudioFireplace.pause()
+    }
+
+    return {
+        pressButton,
+        timeEnd,
+        audioForest,
+        audioRain,
+        audioCoffeeShop,
+        audioFireplace
+    }
+}
